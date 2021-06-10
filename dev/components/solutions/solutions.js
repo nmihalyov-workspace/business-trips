d086beaf06e1517ddfeda5cad26e478f7ce85e01
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2021-06-10 13:06:52 +0300

    Main page adaptation

--- dev/components/solutions/solutions.js
+++ dev/components/solutions/solutions.js
@@ -9,3 +9,22 @@ Array.from(document.querySelectorAll('.solutions__tab')).map(el => {
     document.querySelector(`.solutions__content[data-tab="${target}"]`).classList.add('solutions__content--active');
   });
 });
+
+Array.from(document.querySelectorAll('.solutions__select-item')).map(el => {
+  el.addEventListener('click', e => {
+    const element = e.currentTarget;
+    const currentSelectItem = element.parentNode.previousElementSibling;
+    const target = element.dataset.tab;
+    const select = document.querySelector('.solutions__select');
+
+    if (select.classList.contains('solutions__select--active') && currentSelectItem.classList.contains('solutions__select-item')) {
+      element.parentNode.prepend(currentSelectItem);
+      element.parentNode.parentNode.prepend(element);
+    }
+
+    select.classList.toggle('solutions__select--active');
+    
+    document.querySelector('.solutions__content--active').classList.remove('solutions__content--active');
+    document.querySelector(`.solutions__content[data-tab="${target}"]`).classList.add('solutions__content--active');
+  });
+});
